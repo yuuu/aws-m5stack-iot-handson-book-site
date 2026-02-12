51593815945bcf5648b79ce4f3e803dc112633ca
yuuu <okazaki@fusic.co.jp>
2026-02-13 05:44:34 +0900

    linter and formatter

--- cloud/env-sensor-backend/src/dashboard/app.rb
+++ cloud/env-sensor-backend/src/dashboard/app.rb
@@ -1,19 +1,19 @@
 # frozen_string_literal: true
 
-require "erb"
-require "json"
+require 'erb'
+require 'json'
 
 def handler(event:, context:)
   @temperature = 25.1
   @humidity = 45.5
   @pressure = 1012.8
 
-  template = File.read(File.join(__dir__, "index.html.erb"))
+  template = File.read(File.join(__dir__, 'index.html.erb'))
   body = ERB.new(template).result(binding)
 
   {
     statusCode: 200,
-    headers: { "Content-Type": "text/html" },
-    body: body,
+    headers: { 'Content-Type': 'text/html' },
+    body: body
   }
 end
--- cloud/env-sensor-backend/test/app.rb
+++ cloud/env-sensor-backend/test/app.rb
@@ -1,19 +1,19 @@
 # frozen_string_literal: true
 
-require "minitest/autorun"
-require_relative "../src/dashboard/app"
+require 'minitest/autorun'
+require_relative '../src/dashboard/app'
 
 class TestApp < Minitest::Test
   def setup
-    ENV["SENSOR_READINGS_TABLE"] = "test_table"
+    ENV['SENSOR_READINGS_TABLE'] = 'test_table'
     @client = Aws::DynamoDB::Client.new(stub_responses: true)
   end
 
   def test_get_all_items
     @client.stub_responses(:scan, {
                              items: [
-                               { "id" => "123", "name" => "Test Item" },
-                               { "id" => "456", "name" => "Test Item 2" }
+                               { 'id' => '123', 'name' => 'Test Item' },
+                               { 'id' => '456', 'name' => 'Test Item 2' }
                              ]
                            })
 
@@ -22,8 +22,8 @@ class TestApp < Minitest::Test
     assert_equal 200, response[:statusCode]
     assert_equal(
       [
-        { "id" => "123", "name" => "Test Item" },
-        { "id" => "456", "name" => "Test Item 2" }
+        { 'id' => '123', 'name' => 'Test Item' },
+        { 'id' => '456', 'name' => 'Test Item 2' }
       ].to_json, response[:body]
     )
   end
